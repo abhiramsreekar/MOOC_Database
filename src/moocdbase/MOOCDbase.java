e1f10e54192309eed17901d13733cd95ec754d79
Abhiram Sreekar <83418471+abhiramsreekar@users.noreply.github.com>
2022-06-26 19:50:03 +0530

    Add files via upload

---
+++ src/moocdbase/MOOCDbase.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moocdbase;
+
+/**
+ *
+ * @author abhir
+ */
+public class MOOCDbase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TheMainPage mp=new TheMainPage();
+        mp.show();
+    }
+    
+}
